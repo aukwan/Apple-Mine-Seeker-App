a4e478c89360a12f65fffa2b468b6aae105d2844
aukwan <austinkwan32@gmail.com>
2020-10-09 10:32:30 -0700

    initial commit

---
+++ app/src/main/java/ca/sfu/cmpt276/apple_mine_seeker/MainActivity.java
@@ -0,0 +1,14 @@
+package ca.sfu.cmpt276.apple_mine_seeker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
