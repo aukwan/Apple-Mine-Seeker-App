9f0bf6b5350880cc7e3031cd36fd32b17d79d854
aukwan <austinkwan32@gmail.com>
2020-10-16 02:55:31 -0700

    welcome activity done

---
+++ app/docs/readme.txt
@@ -0,0 +1,2 @@
+Optional Features:
+- Welcome Screen animations and automatically advancing to Main Menu after 4s
--- app/src/main/java/ca/sfu/cmpt276/apple_mine_seeker/ui/WelcomeActivity.java
+++ app/src/main/java/ca/sfu/cmpt276/apple_mine_seeker/ui/WelcomeActivity.java
@@ -4,14 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import ca.sfu.cmpt276.apple_mine_seeker.R;
 
 public class WelcomeActivity extends AppCompatActivity {
 
-    Button skip;
+    Button skipBtn;
+    ImageView mineApple;
+    ImageView apple1;
+    ImageView apple2;
+    Animation rotate;
+    Animation fadeIn;
+    private static long POST_ANIM_DELAY = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +29,12 @@ public class WelcomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_welcome);
 
         setupSkipButton();
+        startAnimation();
     }
 
     private void setupSkipButton() {
-        skip = findViewById(R.id.skipBtn);
-        skip.setOnClickListener(new View.OnClickListener() {
+        skipBtn = findViewById(R.id.skipBtn);
+        skipBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), MainMenuActivity.class);
@@ -31,5 +42,49 @@ public class WelcomeActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    private void startAnimation() {
+        mineApple = findViewById(R.id.mineApple);
+        apple1 = findViewById(R.id.apple1);
+        apple2 = findViewById(R.id.apple2);
+
+        rotate = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate);
+        fadeIn = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in);
+
+        apple1.setAnimation(fadeIn);
+        apple2.setAnimation(fadeIn);
+        mineApple.setAnimation(rotate);
+
+        rotate.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                // Sample handler code from Stack Overflow: https://stackoverflow.com/a/29148136
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent = new Intent(getApplicationContext(), MainMenuActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                }, POST_ANIM_DELAY);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finishAffinity();
     }
 }
